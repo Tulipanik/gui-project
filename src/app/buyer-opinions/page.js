5a861d2b32d1d7adbe91615151f753e3f32f6ed3
Kacprucha <karas.kacper@gmail.com>
2024-01-21 20:01:28 +0100

    Adding opinion view

--- src/app/buyer-opinions/page.js
+++ src/app/buyer-opinions/page.js
@@ -1,3 +1,14 @@
+import OpinionTable from "../components/opinionTable";
+import MenuSimple from "../components/sales-chart/dropdown";
+
 export default function BuyerOpinions() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="mx-auto w-4/5">
+        Opinion type: <span></span>
+        <MenuSimple options={["positive", "negative"]} />
+      </div>
+      <OpinionTable />
+    </div>
+  );
 }
---
+++ src/app/components/opinionTable.js
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { useState } from 'react';
+import {allOpinions} from "../mock-up/opinions"
+
+export default function OpinionTable() {
+  const [opinions, setOpinions] = useState([...allOpinions].sort((a, b) => b.stars - a.stars));
+
+  return (
+    <table className="mx-auto w-4/5 border-2 border-solid border-orange-400 text-left">
+      <thead className="bg-orange-200">
+        <th className="border-solid border-2 border-orange-400 py-2 font-thin">
+          Nr.
+        </th>
+        <th className="border-solid border-2 border-orange-400 font-thin">
+          Opinion
+        </th>
+        <th className="border-solid border-2 border-orange-400 font-thin">
+          Date of publishing
+        </th>
+        <th className="border-solid border-2 border-orange-400 font-thin">
+          Stars given
+        </th>
+      </thead>
+      <tbody className="h-4/5">
+      {opinions.map((opinion, index) => (
+          <tr key={opinion.id}>
+            <td className="border-solid border-2 border-orange-400 py-2 font-thin">{index + 1}</td>
+            <td className="border-solid border-2 border-orange-400 font-thin">{opinion.opinion}</td>
+            <td className="border-solid border-2 border-orange-400 font-thin">{opinion.date}</td>
+            <td className="border-solid border-2 border-orange-400 font-thin">{opinion.stars + " / 5"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
