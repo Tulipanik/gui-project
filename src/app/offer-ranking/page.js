75b120a35a647fc354a2239468088d5aa96bf11d
Kacprucha <karas.kacper@gmail.com>
2024-01-21 20:04:12 +0100

    Add dropdown menu to offer ranking view

--- src/app/offer-ranking/page.js
+++ src/app/offer-ranking/page.js
@@ -1,9 +1,11 @@
 import Top5Table from "../components/offer-ranking/top5Table";
+import MenuSimple from "../components/sales-chart/dropdown";
 
 export default function OfferRanking() {
   return (
     <div className="flex flex-col w-full h-full items-center">
       <h1 className="text-2xl">Top 5</h1>
+      <MenuSimple options={["turnover", "number of sales"]} /*setStatus={setSelectedMeasure()}*/ />{" "}
       <Top5Table />
     </div>
   );
