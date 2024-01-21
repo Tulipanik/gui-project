04f0ff3af354f5ebb479c20bdce50ae40b0cf0f7
Kacprucha <karas.kacper@gmail.com>
2024-01-21 20:02:30 +0100

    Adding name of ofer in 5 top oferts view

--- src/app/components/offer-ranking/mostlyBuyOptions.js
+++ src/app/components/offer-ranking/mostlyBuyOptions.js
@@ -1,7 +1,7 @@
-export default function MostlyBuyOptions({ picesSold, turnover }) {
+export default function MostlyBuyOptions({ name, picesSold, turnover }) {
   return (
     <div className="flex bg-orange-100 h-1/6 mx-auto mx-2 mt-5">
-      <div className="w-3/4 h-full"></div>
+      <div className="w-3/4 h-full">{name}</div>
       <div className="flex flex-col justify-center border-l-4 border-orange-400 my-2 p-2">
         <div>
           Number of pieces sold: <span>{picesSold}</span>
--- src/app/components/offer-ranking/top5Table.js
+++ src/app/components/offer-ranking/top5Table.js
@@ -9,11 +9,11 @@ export default function Top5Table() {
 
   return (
     <div className="bg-orange-400 w-10/12 h-5/6 p-5 px-1 pb-1">
-      <MostlyBuyOptions picesSold={offerts[0].numberOfSoldCopies} turnover={offerts[0].turnover}/>
-      <MostlyBuyOptions picesSold={offerts[1].numberOfSoldCopies} turnover={offerts[1].turnover}/>
-      <MostlyBuyOptions picesSold={offerts[2].numberOfSoldCopies} turnover={offerts[2].turnover}/>
-      <MostlyBuyOptions picesSold={offerts[3].numberOfSoldCopies} turnover={offerts[3].turnover}/>
-      <MostlyBuyOptions picesSold={offerts[4].numberOfSoldCopies} turnover={offerts[4].turnover}/>
+      <MostlyBuyOptions name={offerts[0].productName} picesSold={offerts[0].numberOfSoldCopies} turnover={offerts[0].turnover}/>
+      <MostlyBuyOptions name={offerts[1].productName} picesSold={offerts[1].numberOfSoldCopies} turnover={offerts[1].turnover}/>
+      <MostlyBuyOptions name={offerts[2].productName} picesSold={offerts[2].numberOfSoldCopies} turnover={offerts[2].turnover}/>
+      <MostlyBuyOptions name={offerts[3].productName} picesSold={offerts[3].numberOfSoldCopies} turnover={offerts[3].turnover}/>
+      <MostlyBuyOptions name={offerts[4].productName} picesSold={offerts[4].numberOfSoldCopies} turnover={offerts[4].turnover}/>
     </div>
   );
 }
