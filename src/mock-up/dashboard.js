20a4b31563ce4b7dd3ba08ebb4bfb42529bab052
Kacprucha <karas.kacper@gmail.com>
2024-01-22 20:11:57 +0100

    Adding mocks for dashboard

---
+++ src/mock-up/dashboard.js
@@ -0,0 +1,20 @@
+export const orders = [
+    { id: 1, orderID: 'Super Mario 64 DS / 154780', status: 'unpaid'},
+    { id: 2, orderID: 'PlayStation 2 / 358127', status: 'in delivery'},
+    { id: 3, orderID: 'Zelda 2 / 029130', status: 'paid'},
+];
+
+export const opinions = [
+    { id: 1, opinion: 'Bardzo zadowolony z zakupów, ale czas wysyłki mógłby być ...', stars: 4.5},
+    { id: 2, opinion: 'Trochę zawiedziony jakością gry, którą otrzymałem. Mimo ... ', stars: 3},
+    { id: 3, opinion: 'Fantastyczny sklep z grami retro! Znalazłem tu wiele ...', stars: 5},
+    { id: 4, opinion: 'Zamówiłem grę, ale niestety nie otrzymałem jej nawet po ...', stars: 1}
+];
+
+export const ranking = [
+    { id: 1, name: 'Nintendo 3DS (ニンテンドー3DS Nintendō Surīdīesu) - 800 zł ...', turnover:249800},
+    { id: 2, name: 'Wii - konsola plus kontrolwer - 250 zł stan lekko używany', turnover:101300},
+    { id: 3, name: 'Mario Party DS - 2007 - 80 zł stan lekko używany', turnover:12000},
+    { id: 4, name: 'PlayStation 3 (PS3) - 2016 - konsola + 2 pady - 600 zł stan ...', turnover:5000},
+    { id: 5, name: 'Xbox 360 Microsoft pad - 50 zł stan używany', turnover:4500}
+];
