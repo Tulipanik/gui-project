8bcdbd73e9e7b80270d30d15f452beb6b5413889
Kacprucha <karas.kacper@gmail.com>
2024-01-22 19:22:35 +0100

    Logic to drop dwon menu choosing

--- src/app/buyer-opinions/page.js
+++ src/app/buyer-opinions/page.js
@@ -1,14 +1,37 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import OpinionTable from "@/components/opinionTable";
 import MenuSimple from "@/components/sales-chart/dropdown";
+import { allOpinions } from "@/mock-up/opinions";
 
 export default function BuyerOpinions() {
+  const [selectedOption, setSelectedOption] = useState("all");
+  const [opinions, setOpinions] = useState(allOpinions);
+
+  useEffect(() => {
+    console.log("filtruje:");
+    switch (selectedOption) {
+      case "all":
+        setOpinions(allOpinions);
+        break;
+      case "positive":
+        setOpinions(allOpinions.filter(obj => obj.stars > 3));
+        break;
+      case "negative":
+        setOpinions(allOpinions.filter(obj => obj.stars <= 3));
+        break;
+    };
+    console.log(opinions)
+  }, [selectedOption]);
+
   return (
     <div>
-      <div className="mx-auto w-4/5">
+      <div className="mx-auto w-4/5 flex my-5">
         Opinion type: <span></span>
-        <MenuSimple options={["positive", "negative"]} />
+        <MenuSimple options={["all", "positive", "negative"]} setStatus={setSelectedOption} />
       </div>
-      <OpinionTable />
+      <OpinionTable opinions={opinions} />
     </div>
   );
 }
--- src/app/offer-ranking/page.js
+++ src/app/offer-ranking/page.js
@@ -1,7 +1,27 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Top5Table from "@/components/offer-ranking/top5Table";
 import MenuSimple from "@/components/sales-chart/dropdown";
+import {bestFiveOfertsByTurnOver, bestFiveOfertsByVeiws} from "../../mock-up/ranking"
 
 export default function OfferRanking() {
+  const [selectedOption, setSelectedOption] = useState("turnover");
+  const [offers, setOffers] = useState(bestFiveOfertsByTurnOver);
+
+  useEffect(() => {
+    console.log("filtruje:");
+    switch (selectedOption) {
+      case "turnover":
+        setOffers(bestFiveOfertsByTurnOver);
+        break;
+      case "number of sales":
+        setOffers(bestFiveOfertsByVeiws);
+        break;
+    };
+    console.log(offers)
+  }, [selectedOption]);
+
   return (
     <div className="flex flex-col w-full h-full items-center">
       <h1 className="text-2xl">Top 5</h1>
@@ -9,9 +29,9 @@ export default function OfferRanking() {
         options={[
           "turnover",
           "number of sales",
-        ]} /*setStatus={setSelectedMeasure()}*/
+        ]} setStatus={setSelectedOption}
       />{" "}
-      <Top5Table />
+      <Top5Table offers={offers} measurment={selectedOption}/>
     </div>
   );
 }
